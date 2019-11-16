94753792d3aca331fb7c0f2c306f5b6d99964fed
appdevdude7 <appdeveloper.akshay@gmail.com>
2019-11-16 07:59:52 -0600

    about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => (
  <div>
  <h1>This Is From About Us Page</h1>
+ <p>This is a CRUD app to use CRUD features with the help of ReactJS and JavaScript</p>
  </div>
 )
 
